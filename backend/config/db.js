91f7b9a03b295e206fe7dfc4d0884404ff16e702
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-08-27 16:17:54 +0530

    db configuration

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Connect to MongoDB
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the process with failure
+  }
+
+}
+
+module.exports = connectDB;
