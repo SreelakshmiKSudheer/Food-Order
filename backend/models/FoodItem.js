5218f2b8ec8a9474cebcda9af290114ccb38f556
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-10-02 22:55:23 +0530

    model update

--- backend/models/FoodItem.js
+++ backend/models/FoodItem.js
@@ -5,9 +5,16 @@ const FoodItemSchema = new mongoose.Schema({
     description: { type: String },
     image: { type: String, required: true },
     price: { type: Number, required: true },
-    category: { type: String, required: true },
-    type: { type: String, required: true },
-    ingredients: [{ type: String, required: true }]
+    category: { 
+        type: String, 
+        required: true, 
+        enum: ['Breakfast', 'Lunch', 'Snack', 'Dinner'] 
+    },
+    type: { type: String, required: true, enum: ['Veg', 'Vegan', 'NonVeg']  },
+    ingredients: [{ type: String, required: true }],
+    seller: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('FoodItem', FoodItemSchema);
