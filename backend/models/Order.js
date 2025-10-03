69121e15baaaaa9c0b336e3bd7d4dc0cab6b687b
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-10-03 17:52:19 +0530

    model change

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    seller: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     foodItem: { type: Schema.Types.ObjectId, ref: 'FoodItem', required: true },
+    quantity: { type: Number, required: true },
     orderDate: { type: Date, default: Date.now },
     paymentStatus: { type: String, enum: ['pending', 'completed', 'failed'], default: 'pending' },
     orderStatus: { type: String, enum: ['placed', 'preparing', 'delivered', 'cancelled'], default: 'placed' }
