4252e66bc3bf6c875209651c6d23b65288188d29
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-11-03 13:02:05 +0530

    name changed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { CircleUserRound } from 'lucide-react'
 const Header = () => {
 return (
     <header className="flex justify-between items-center p-4">
-        <h1 className="m-0 text-2xl font-bold text-amber-600">Fast II Order</h1>
+        <h1 className="m-0 text-2xl font-bold text-amber-600">Fast Order</h1>
         <nav className="flex gap-5">
             <a href="/profile" className="mr-4 hover:underline"><CircleUserRound /></a>
             <a href="/about" className="hover:underline">About</a>
